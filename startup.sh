5bef17f9eeb1df4bd978534c91db9f78b90e2516
Pomax <pomax@nihongoresources.com>
2017-03-03 12:06:46 -0800

    cd?

--- startup.sh
+++ startup.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+# This is really funny, but necessary
+cd cmstest
+
 # Make Django migrations, just in case.
-python cmstest/manage.py makemigrations
+python manage.py makemigrations
 
 # Then migrate up, again just in case.
-python cmstest/manage.py migrate
+python manage.py migrate
 
 # Finally, start up the system.
-python cmstest/manage.py runserver "0.0.0.0:$PORT"
+python manage.py runserver "0.0.0.0:$PORT"
