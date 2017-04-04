f7c367ab0341a18b751e519602103f442c96c91b
Alan Mooiman <amooiman@gmail.com>
2017-04-04 16:57:56 -0400

    React component integration

--- network/admin.py
+++ network/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import PageWithCallout, Signup, FooterLink
+from .models import PageWithCallout, Signup, FooterLink, BasketNewsletter
 
 admin.site.register(PageWithCallout)
 admin.site.register(Signup)
 admin.site.register(FooterLink)
+admin.site.register(BasketNewsletter)
