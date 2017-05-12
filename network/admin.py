8e070943ec1544ad1f6e90f214fdd1b80471fa46
Tasos Katsoulas <akatsoulas@gmail.com>
2017-05-12 14:11:42 +0300

    Remove network and networkapi apps.

--- network/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import PageWithCallout, Signup, FooterLink, BasketNewsletter
-
-admin.site.register(PageWithCallout)
-admin.site.register(Signup)
-admin.site.register(FooterLink)
-admin.site.register(BasketNewsletter)
