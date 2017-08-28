d09aee654063b01fe12e39bb25df58e5ca91973e
Nikos Roussos <nikos@roussos.cc>
2017-08-28 20:49:28 +0300

    Initial implementation of top-level navigation menu

---
+++ coss/home/static/js/coss.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    'use strict';
+
+    // Toggle height on collapsed menu
+    $('#nav-top button.navbar-toggler').click(function() {
+        $('#navbarContent').toggleClass('show');
+    });
+});
