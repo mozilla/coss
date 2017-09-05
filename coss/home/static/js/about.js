c40f2719b0b90cad604730cdbd79a91e3e789299
Nikos Roussos <nikos@roussos.cc>
2017-09-05 13:11:45 +0300

    About page UI refinement
    * Make details block dynamic (Fixes #340)
    * CSS adjustments to meet Phase 1 design (Fixes #335, Fixes #332)

---
+++ coss/home/static/js/about.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    'use strict';
+
+    // Smooth scrolling on nav links
+    $('#nav-about a.nav-link[href*="#"]').click(function() {
+        var target = $(this.hash);
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        });
+    });
+
+    // Details block nav
+    $('#nav-details a.nav-link[href*="#"]').click(function(event) {
+        event.preventDefault();
+        var target = $(this.hash);
+        $('#nav-details a.nav-link').removeClass('active');
+        $(this).addClass('active');
+        $('#details .nav-content').hide();
+        $(target).show();
+    });
+
+    // Pick background image
+    var bg_image = $('#body').data('bg-img');
+    if (bg_image) {
+        $('.body-image').css('background-image', 'url(' + bg_image + ')');
+    } else {
+        $('.body-image').css('background-image', 'url(/static/img/alex-wong.jpg)');
+    }
+});
