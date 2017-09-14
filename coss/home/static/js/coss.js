6f8c75926866b072339163b4d098ca65d8b6ceca
Nikos Roussos <nikos@roussos.cc>
2017-09-14 15:39:39 +0300

    Fix mobile navbar interaction

--- coss/home/static/js/coss.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    'use strict';
-
-    // Toggle height on collapsed menu
-    $('#nav-top button.navbar-toggler').click(function() {
-        $('#navbarContent').toggleClass('show');
-    });
-});
